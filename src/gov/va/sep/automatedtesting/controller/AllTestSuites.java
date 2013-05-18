9e79106dabf11297838e4d6ff7ae008cdd04021e
Andal FeQuiere <andal.fequiere@us.pwc.com>
2013-05-17 22:26:28 -0400

    first push of project

---
+++ src/gov/va/sep/automatedtesting/controller/AllTestSuites.java
@@ -0,0 +1,20 @@
+package gov.va.sep.automatedtesting.controller;
+
+import gov.va.sep.automatedtesting.suites.*;
+
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+	/*VeteranP1211.class ,
+	VeteranP1011.class,
+	BBMIgmail.class,
+	Directemail.class,	
+	BBMILookAndFeel.class,
+	VeteranNegative.class,
+	DirectemailErrorMsg.class*/})
+public final class AllTestSuites {
+
+}
